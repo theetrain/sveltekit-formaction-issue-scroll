f3bc074a8640dcb8ebbee2a1730929c904b283cc
Enrico Sacchetti <enrico@theetrain.ca>
2023-03-19 22:53:03 -0400

    chore: add form example

---
+++ src/routes/+page.server.js
@@ -0,0 +1,34 @@
+export const actions = {
+  addPet: async ({ request }) => {
+    const formItems = await request.formData()
+    console.log({ formItems })
+
+    const items = [...formItems.entries()]
+
+    /**
+     * The pets.
+     */
+    const pets = [...formItems.getAll('petname[]'), '']
+
+    return { items, pets }
+  },
+  removePet: async ({ request, url }) => {
+    const formItems = await request.formData()
+    const pet = parseInt(String(url.searchParams.get('pet')), 10) || 0
+    console.log({ pet })
+
+    const items = [...formItems.entries()]
+    const pets = [...formItems.getAll('petname[]')]
+
+    if (pet > 0) {
+      pets.splice(pet, pet)
+    }
+
+    return { items, pets }
+  },
+  submit: async () => {
+    console.log('submitted!')
+
+    return { success: true }
+  },
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { enhance } from '$app/forms'
+
+  /** @type {import('./$types').ActionData} */
+  export let form
+  $: console.log({ form })
+</script>
+
+<svelte:head>
+  <title>Dynamic form!</title>
+  <link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css" />
+</svelte:head>
+
+<h1>Dynamic form</h1>
+
+{#if form?.success === true}
+  <p><strong>You did it!</strong></p>
+{/if}
+
+<form
+  id="petform"
+  method="POST"
+  action="?/submit"
+  use:enhance
+  data-sveltekit-noscroll
+>
+  <label>
+    Your name
+    <input name="name" type="text" value={form?.items?.name ?? ''} />
+  </label>
+
+  <fieldset>
+    <legend>How many pets do you have?</legend>
+
+    <label>
+      <input type="radio" name="pets" value="0" />
+      0
+    </label>
+    <label>
+      <input type="radio" name="pets" value="1" />
+      1
+    </label>
+    <label>
+      <input type="radio" name="pets" value="2" />
+      2
+    </label>
+    <label>
+      <input type="radio" name="pets" value="3" />
+      3+
+    </label>
+  </fieldset>
+  <br />
+  {#each form?.pets || [''] as pet, i}
+    <label for="petname-{i}">Pet #{i + 1}'s name</label>
+    <input id="petname-{i}" type="text" name="petname[]" value={pet} />
+    <button class="remove" formaction="?/removePet&pet={i}"
+      >Remove pet #{i + 1}</button
+    >
+  {/each}
+  <div>
+    <button style="--color-link: #444;" formaction="?/addPet"
+      >Add pet name</button
+    >
+    <button>Submit form!</button>
+  </div>
+</form>
+
+<style>
+  .remove {
+    padding: 0.5rem;
+    --color-link: darkred;
+  }
+  input:has(+ .remove) {
+    margin-bottom: 0.25rem;
+  }
+  input + .remove {
+    margin-bottom: 1.5rem;
+  }
+</style>
